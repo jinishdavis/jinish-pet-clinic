f9f1e1f159762ecaf70010c0dd4e4e9595f76439
jinishdavis <jinishdavis@gmail.com>
2018-12-08 12:16:17 -0600

    Initial Project Add

---
+++ src/main/java/com/jinishdavis/jinishpetclinic/JinishPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.jinishdavis.jinishpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinishPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinishPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jinishdavis/jinishpetclinic/JinishPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jinishdavis.jinishpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JinishPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
