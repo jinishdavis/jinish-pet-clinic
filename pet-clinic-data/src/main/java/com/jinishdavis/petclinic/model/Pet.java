d8e6ffc98b610a6dc0133d640041b9b2e60b7cea
jinishdavis <jinishdavis@gmail.com>
2018-12-09 20:38:14 -0600

    Add Id to model objects #10. Closes #10.

--- pet-clinic-data/src/main/java/com/jinishdavis/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/jinishdavis/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.jinishdavis.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private  PetType petType;
     private Owner owner;
