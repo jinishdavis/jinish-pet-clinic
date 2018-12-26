968ba643ac89b6d099bb364cc26228413bfb7a8b
jinishdavis <jinishdavis@gmail.com>
2018-12-26 11:20:13 -0600

    Added vets list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/com/jinishdavis/jinishpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/jinishdavis/jinishpetclinic/controllers/OwnerController.java
@@ -1,6 +1,7 @@
 package com.jinishdavis.jinishpetclinic.controllers;
 
 import com.jinishdavis.jinishpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    @Autowired
     private  final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
--- pet-clinic-web/src/main/java/com/jinishdavis/jinishpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jinishdavis/jinishpetclinic/controllers/VetController.java
@@ -1,15 +1,26 @@
 package com.jinishdavis.jinishpetclinic.controllers;
 
+import com.jinishdavis.jinishpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
 public class VetController {
 
-    @RequestMapping({"/vets","vets/index","vets/index.html"})
-    public String listVets(){
+    @Autowired
+    private VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","vets/index","vets/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
